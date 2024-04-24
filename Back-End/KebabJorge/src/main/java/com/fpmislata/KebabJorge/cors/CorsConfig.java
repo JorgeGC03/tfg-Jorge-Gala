80cc30ea66e48f006a55a0d68ae01bedbfa9b1bc
Jorge Gala <jorgegalacaballero@gmail.com>
2024-04-24 20:54:17 +0200

    added angular path to CorsConfig

--- Back-End/KebabJorge/src/main/java/com/fpmislata/KebabJorge/cors/CorsConfig.java
+++ Back-End/KebabJorge/src/main/java/com/fpmislata/KebabJorge/cors/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:8081")
+                    .allowedOrigins("http://localhost:8081", "http://localhost:4200")
                     .allowedMethods("GET", "POST", "PUT", "DELETE")
                     .allowedHeaders("*");
         }
